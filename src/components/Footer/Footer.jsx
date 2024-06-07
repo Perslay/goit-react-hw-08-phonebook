1737a553a25daf3f205bd2d2a0dbccbe0e92de61
Perslay <natalia.44.fedyk@gmail.com>
2024-06-07 23:01:02 +0200

    Footer
    Footer information and some styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,25 @@ export const Footer = () => {
   return (
     <footer className={css.footer}>
       <div className={css.container}>
-        <p>Footer</p>
+        <address className={css.address}>
+          <p className={css.paragraph}>Website created by Natalia Fedyk</p>
+          <p className={css.paragraph}>
+            Email:{' '}
+            <a className={css.link} href="mailto:natalia.44.fedyk@gmail.com">
+              natalia.44.fedyk@gmail.com
+            </a>
+          </p>
+          <p className={css.paragraph}>
+            Backend and react template provided by{' '}
+            <a className={css.link} href="https://github.com/luxplanjay">
+              Alexander Repeta
+            </a>{' '}
+            and{' '}
+            <a className={css.link} href="https://goit.global/ua/">
+              GOIT Academy
+            </a>
+          </p>
+        </address>
       </div>
     </footer>
   );
